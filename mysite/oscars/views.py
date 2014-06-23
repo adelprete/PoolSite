c9a3fd3d3735038c170c3a058f3740e3528b7c48
Anthony <adelprete87@gmail.com>
2014-06-23 10:34:21 -0400

    Moved the open pools chaeck up in all views

--- mysite/oscars/views.py
+++ mysite/oscars/views.py
@@ -208,11 +208,15 @@ def pool_standings(request,id=None):
 @login_required
 def oscar_pool(request,id=None,form_class=oforms.OscarPoolForm):
 
+    if settings.OSCAR_POOLS_OPEN is False:
+        messages.error(request,"You've missed your chance to create a pool.  Come back when the Nominees are announced for next year's award show.")
+        return HttpResponseRedirect(reverse('root'))
+
     ceremony = omodels.OscarCeremony.objects.latest('date')
 
     if not id:
         today = datetime.datetime.utcnow()
-        if settings.OSCAR_POOLS_OPEN is False or datetime.timedelta(0) > (ceremony.date.replace(tzinfo=None) - today):
+        if datetime.timedelta(0) > (ceremony.date.replace(tzinfo=None) - today):
             messages.error(request,"You've missed your chance to create a pool.  Come back when the Nominees are announced for next year's award show.")
             return HttpResponseRedirect(reverse('root'))
 
--- mysite/survivor/views.py
+++ mysite/survivor/views.py
@@ -34,6 +34,10 @@ def pool_homepage(request,id=None):
 @login_required
 def survivor_pool(request,id=None,form=sforms.SurvivorPoolForm):
 
+    if settings.SURVIVOR_POOLS_OPEN is False:
+        messages.error(request,"You've missed your chance to create a Survivor pool.  Come back when the cast is revealed for the next season.")
+        return HttpResponseRedirect(reverse('root'))
+
     season = smodels.SurvivorSeason.objects.latest('start_date')
 
     survivor_pool = None
@@ -41,7 +45,7 @@ def survivor_pool(request,id=None,form=sforms.SurvivorPoolForm):
 
     if not id:
         today = datetime.datetime.utcnow()
-        if settings.SURVIVOR_POOLS_OPEN is False or datetime.timedelta(0) > (season.start_date.replace(tzinfo=None) - today):
+        if datetime.timedelta(0) > (season.start_date.replace(tzinfo=None) - today):
             messages.error(request,"You've missed your chance to create a Survivor pool.  Come back when the cast is revealed for the next season.")
             return HttpResponseRedirect(reverse('root'))
 
