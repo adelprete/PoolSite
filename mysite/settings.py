4dcd08a63b4b07749421164c310adfb83c3208ff
Anthony <adelprete87@gmail.com>
2014-06-27 08:59:02 -0400

    forgot a comma in allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ DEFAULT_FROM_EMAIL = "OfficePoolHub <no-reply@officepoolhub.com>"
 ALLOWED_HOSTS = [
     'www.officepoolhub.com',
     '127.0.0.1:9001',
-    '50.116.20.10'
+    '50.116.20.10',
     '127.0.0.1',
 ]
 
