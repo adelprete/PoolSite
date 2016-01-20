9509045ad1c54047a71817aea0aa4fec614be0d5
Anthony D <adelprete87@gmail.com>
2016-01-19 22:25:56 -0500

    Fixed how wins were counted on profiles

--- mysite/base/views/__init__.py
+++ mysite/base/views/__init__.py
@@ -98,19 +98,19 @@ def profile_stats(request,id=None):
     member = profile.user
 
     history={}
-    survivorpools = smodels.SurvivorPool.objects.filter(Q(administrator=member)|Q(members=member))
+    survivorpools = smodels.SurvivorPool.objects.filter(Q(administrator=member)|Q(members=member)).distinct()
     history['Survivor'] = (survivorpools.filter(winner=member).count(),survivorpools.filter(second_place=member).count(),survivorpools.filter(third_place=member).count())
-
-    oscarpools = omodels.OscarPool.objects.filter(Q(administrator=member)|Q(members=member))
+    
+    oscarpools = omodels.OscarPool.objects.filter(Q(administrator=member)|Q(members=member)).distinct()
     history['Oscars'] = (oscarpools.filter(winner=member).count(),oscarpools.filter(second_place=member).count(),oscarpools.filter(third_place=member).count())
 
-    amazingpools = amodels.AmazingRacePool.objects.filter(Q(administrator=member)|Q(members=member))
+    amazingpools = amodels.AmazingRacePool.objects.filter(Q(administrator=member)|Q(members=member)).distinct()
     history['Amazing Race'] = (amazingpools.filter(winner=member).count(),amazingpools.filter(second_place=member).count(),amazingpools.filter(third_place=member).count())
 
-    nflpools = nflsmodels.NFLSurvivorPool.objects.filter(Q(administrator=member)|Q(members=member))
+    nflpools = nflsmodels.NFLSurvivorPool.objects.filter(Q(administrator=member)|Q(members=member)).distinct()
     history['NFL Survivor'] = (nflpools.filter(winner=member).count(),nflpools.filter(second_place=member).count(),nflpools.filter(third_place=member).count())
 
-    marchpools = mmodels.MarchMadnessPool.objects.filter(Q(administrator=member)|Q(members=member))
+    marchpools = mmodels.MarchMadnessPool.objects.filter(Q(administrator=member)|Q(members=member)).distinct()
     history['March Madness'] = (marchpools.filter(winner=member).count(),marchpools.filter(second_place=member).count(),marchpools.filter(third_place=member).count())
 
 
